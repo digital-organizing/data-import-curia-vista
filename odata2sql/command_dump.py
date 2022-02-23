66750f0d515c3d6f82396c6f708d027856e74db2
Reto Schneider <code@reto-schneider.ch>
2022-02-23 02:59:41 +0100

    dump: Show multiplicity of principal

--- odata2sql/command_dump.py
+++ odata2sql/command_dump.py
@@ -2,7 +2,7 @@ from odata2sql.odata import Context
 
 
 def work(odata: Context, args):
-    for entity_type in odata.included_entity_types:
+    for entity_type in sorted(odata.included_entity_types, key=lambda et: et.name):
         print(f' - {entity_type.name}:')
         print(f'   - Columns:')
         for p in entity_type.proprties():
@@ -10,6 +10,9 @@ def work(odata: Context, args):
                   f' fixed_length={p.fixed_length}')
         if entity_type_targets := odata.get_dependencies(entity_type, recursive=False):
             print(f'   - Directly depends on: ' + ', '.join(e.name for e in entity_type_targets))
+        if multiplicity := list(odata.get_dependency_with_multiplicity(entity_type)):
+            print(f'   - Directly depends on: ' + ', '.join(
+                [f'{m.principal.name} ({m.principal_multiplicity})' for m in multiplicity]))
     if args.ipython:
         import IPython
         IPython.embed(colors="neutral")
